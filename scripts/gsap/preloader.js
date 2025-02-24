89d09548cdfc911f351b79a25ad06f3d8a504715
Egor <165127289+fer1mka@users.noreply.github.com>
2025-02-24 20:54:04 +0300

    Update preloader.js

--- scripts/gsap/preloader.js
+++ scripts/gsap/preloader.js
@@ -2,5 +2,6 @@ window.addEventListener('load', () => {
 	const preloader = document.querySelector('.loader')
 	if(preloader){
 		preloader.classList.add('done')
+		setTimeout(() => preloader.parentElement.removeChild(preloader), 1000)
 	}
 })
